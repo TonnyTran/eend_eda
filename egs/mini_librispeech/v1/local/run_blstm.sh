f47aff01fb5af29b19fe0f30e58eb66fdc4b304d
TonnyTran <anhtt17895@gmail.com>
2023-06-22 18:58:07 +0800

    eend_eda for DIHARD3

--- egs/mini_librispeech/v1/local/run_blstm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copyright 2019 Hitachi, Ltd. (author: Yusuke Fujita)
-# Licensed under the MIT license.
-#
-# BLSTM-based model experiment
-./run.sh --train-config conf/blstm/train.yaml --infer-config conf/blstm/infer.yaml $*
